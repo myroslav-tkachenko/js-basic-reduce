61246b8d663e1c38ac93351223ff0bf110112724
mirkinoid <myroslav.tkachenko@gmail.com>
2018-06-23 02:23:03 +0300

    Return acc for an empty list

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const _ = {};
 _.reduce = (l, f, a) => {
-  if (l.length === 0) return l;
+  if (l.length === 0) return a;
   if (l.length === 1) return f(...l, a);
   return _.reduce(l.slice(1), f, f(l[0], a));
 };
 
-document.write(_.reduce([1], (e, a) => e * a, 1));
+document.write(_.reduce([1, 2, 3, 4], (e, a) => e * a, 1));
